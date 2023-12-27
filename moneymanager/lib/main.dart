ad4d22578bf7b708bb435435e95da2ac7ecfa15a
Dang Quang Vi <vidangquang1203@gmail.com>
2023-12-27 07:07:58 +0700

    order food

--- moneymanager/lib/main.dart
+++ moneymanager/lib/main.dart
@@ -1,120 +1,588 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    Color color = Theme.of(context).primaryColor;
-
-    Widget buttonSection = Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        _buildButtonColumn(color, Icons.call, 'CALL'),
-        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
-        _buildButtonColumn(color, Icons.message, 'MESSAGE'),
-        _buildButtonColumn(color, Icons.share, 'SHARE'),
-      ],
+    return const MaterialApp(
+      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
     );
+  }
+}
 
-    Widget textSection = Container(
-      padding: const EdgeInsets.all(32),
-      child: const Text(
-        'Phenikaa University was established on 10 October 2007, and became a member of Phenikaa Group - Vietnam’s leading industrial corporation - in 2017. \nPhenikaa University strives to become a multidisciplinary university par excellence in research, academic, and career development based on international standards in training, scientific research, and university governance.',
-        textAlign: TextAlign.justify,
-        softWrap: true,
-      ),
-    );
+@immutable
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
 
-    return MaterialApp(
-      title: 'PKA layout demo',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('PKA layout demo'),
-        ),
-        body: ListView(
-          children: [
-            Image.asset(
-              './images/phenikaaLogo.jpg',
-              width: 600,
-              height: 240,
-              fit: BoxFit.cover,
-            ),
-            titleSection,
-            buttonSection,
-            textSection
-          ],
-        ),
-      ),
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+  final List<Customer> _people = [
+    Customer(
+      name: 'Anna',
+      imageProvider: const NetworkImage(
+          'https://flutter.dev/docs/cookbook/img-files/effects/split-check/Avatar1.jpg'),
+    ),
+    Customer(
+      name: 'Nathan',
+      imageProvider: const NetworkImage(
+          'https://flutter.dev/docs/cookbook/img-files/effects/split-check/Avatar2.jpg'),
+    ),
+    Customer(
+      name: 'Emilio',
+      imageProvider: const NetworkImage(
+          'https://flutter.dev/docs/cookbook/img-files/effects/split-check/Avatar3.jpg'),
+    ),
+  ];
+
+  final GlobalKey _draggableKey = GlobalKey();
+
+  void _itemDroppedOnCustomerCart({
+    required Item item,
+    required Customer customer,
+  }) {
+    setState(() {
+      customer.items.add(item);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFF7F7F7),
+      appBar: _buildAppBar(),
+      body: _buildContent(),
     );
   }
 
-  Column _buildButtonColumn(Color color, IconData icon, String label) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(icon, color: color),
-        Container(
-          margin: const EdgeInsets.only(top: 8),
-          child: Text(
-            label,
-            style: TextStyle(
-              fontSize: 12,
-              fontWeight: FontWeight.w400,
-              color: color,
+  PreferredSizeWidget _buildAppBar() {
+    return AppBar(
+      iconTheme: const IconThemeData(color: Color(0xFFF64209)),
+      title: Text(
+        'Order Food',
+        style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+              fontSize: 36,
+              color: const Color(0xFFF64209),
+              fontWeight: FontWeight.bold,
             ),
+      ),
+      backgroundColor: const Color(0xFFF7F7F7),
+      elevation: 0,
+    );
+  }
+
+  Widget _buildContent() {
+    return Stack(
+      children: [
+        SafeArea(
+          child: Column(
+            children: [
+              Expanded(
+                child: _buildMenuList(),
+              ),
+              _buildPeopleRow(),
+            ],
           ),
         ),
       ],
     );
   }
+
+  Widget _buildMenuList() {
+    return ListView.separated(
+      padding: const EdgeInsets.all(16),
+      itemCount: _items.length,
+      separatorBuilder: (context, index) {
+        return const SizedBox(
+          height: 12,
+        );
+      },
+      itemBuilder: (context, index) {
+        final item = _items[index];
+        return _buildMenuItem(
+          item: item,
+        );
+      },
+    );
+  }
+
+  Widget _buildMenuItem({
+    required Item item,
+  }) {
+    return LongPressDraggable<Item>(
+      data: item,
+      dragAnchorStrategy: pointerDragAnchorStrategy,
+      feedback: DraggingListItem(
+        dragKey: _draggableKey,
+        photoProvider: item.imageProvider,
+      ),
+      child: MenuListItem(
+        name: item.name,
+        price: item.formattedTotalItemPrice,
+        photoProvider: item.imageProvider,
+        likeCount: item.likeCount,
+      ),
+    );
+  }
+
+  Widget _buildPeopleRow() {
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 8,
+        vertical: 20,
+      ),
+      child: Row(
+        children: _people.map(_buildPersonWithDropZone).toList(),
+      ),
+    );
+  }
+
+  void _navigateToPaymentPage(BuildContext context, Customer customer) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => PaymentPage(
+            customer: customer,
+            onNextPressed: () => _navigateToPaymentOptionPage(context)),
+      ),
+    );
+  }
+
+  void _navigateToPaymentOptionPage(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => PaymentOptionPage(),
+      ),
+    );
+  }
+
+  Widget _buildPersonWithDropZone(Customer customer) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 6,
+        ),
+        child: DragTarget<Item>(
+          builder: (context, candidateItems, rejectedItems) {
+            return CustomerCart(
+              hasItems: customer.items.isNotEmpty,
+              highlighted: candidateItems.isNotEmpty,
+              customer: customer,
+              onPayPressed: () => _navigateToPaymentPage(context, customer),
+            );
+          },
+          onAccept: (item) {
+            _itemDroppedOnCustomerCart(
+              item: item,
+              customer: customer,
+            );
+          },
+        ),
+      ),
+    );
+  }
 }
 
-Widget titleSection = Container(
-  padding: const EdgeInsets.all(32),
-  child: Row(
-    children: [
-      Expanded(
-        /*1*/
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+class CustomerCart extends StatelessWidget {
+  const CustomerCart({
+    super.key,
+    required this.customer,
+    this.highlighted = false,
+    this.hasItems = false,
+    required this.onPayPressed,
+  });
+
+  final Customer customer;
+  final bool highlighted;
+  final bool hasItems;
+  final VoidCallback onPayPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    final textColor = highlighted ? Colors.white : Colors.black;
+
+    return Transform.scale(
+      scale: highlighted ? 1.075 : 1.0,
+      child: Material(
+        elevation: highlighted ? 8 : 4,
+        borderRadius: BorderRadius.circular(22),
+        color: highlighted ? const Color(0xFFF64209) : Colors.white,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 12,
+            vertical: 24,
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ClipOval(
+                child: SizedBox(
+                  width: 46,
+                  height: 46,
+                  child: Image(
+                    image: customer.imageProvider,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 8),
+              Text(
+                customer.name,
+                style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                      color: textColor,
+                      fontWeight:
+                          hasItems ? FontWeight.normal : FontWeight.bold,
+                    ),
+              ),
+              Visibility(
+                visible: hasItems,
+                maintainState: true,
+                maintainAnimation: true,
+                maintainSize: true,
+                child: Column(
+                  children: [
+                    const SizedBox(height: 4),
+                    Text(
+                      customer.formattedTotalItemPrice,
+                      style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                            color: textColor,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                    ),
+                    const SizedBox(height: 4),
+                    Text(
+                      '${customer.items.length} item${customer.items.length != 1 ? 's' : ''}',
+                      style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                            color: textColor,
+                            fontSize: 12,
+                          ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        ElevatedButton(
+                          onPressed: onPayPressed,
+                          child: const Text('Pay'),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+const List<Item> _items = [
+  Item(
+    name: 'Spinach Pizza',
+    totalPriceCents: 1299,
+    uid: '1',
+    imageProvider: NetworkImage(
+        'https://flutter.dev/docs/cookbook/img-files/effects/split-check/Food1.jpg'),
+    likeCount: 12,
+  ),
+  Item(
+    name: 'Veggie Delight',
+    totalPriceCents: 799,
+    uid: '2',
+    imageProvider: NetworkImage(
+        'https://flutter.dev/docs/cookbook/img-files/effects/split-check/Food2.jpg'),
+    likeCount: 43,
+  ),
+  Item(
+    name: 'Chicken Parmesan',
+    totalPriceCents: 1499,
+    uid: '3',
+    imageProvider: NetworkImage(
+        'https://flutter.dev/docs/cookbook/img-files/effects/split-check/Food3.jpg'),
+    likeCount: 99,
+  ),
+];
+
+class MenuListItem extends StatefulWidget {
+  const MenuListItem({
+    Key? key,
+    this.name = '',
+    this.price = '',
+    required this.likeCount,
+    required this.photoProvider,
+    this.isDepressed = false,
+  }) : super(key: key);
+
+  final String name;
+  final String price;
+  final int likeCount;
+  final ImageProvider photoProvider;
+  final bool isDepressed;
+
+  @override
+  _MenuListItemState createState() => _MenuListItemState();
+}
+
+class _MenuListItemState extends State<MenuListItem> {
+  bool liked = false;
+  int likeCount = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      elevation: 12,
+      borderRadius: BorderRadius.circular(20),
+      child: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
           children: [
-            /*2*/
-            Container(
-              padding: const EdgeInsets.only(bottom: 8),
-              child: const Text(
-                'Phenikaa University',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
+            ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child: SizedBox(
+                width: 120,
+                height: 120,
+                child: Center(
+                  child: AnimatedContainer(
+                    duration: const Duration(milliseconds: 100),
+                    curve: Curves.easeInOut,
+                    height: widget.isDepressed ? 115 : 120,
+                    width: widget.isDepressed ? 115 : 120,
+                    child: Image(
+                      image: widget.photoProvider,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
                 ),
               ),
             ),
-            Text(
-              'Ha Noi, Viet Nam',
-              style: TextStyle(
-                color: Colors.grey[500],
+            const SizedBox(width: 30),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    widget.name,
+                    style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                          fontSize: 18,
+                        ),
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    widget.price,
+                    style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                  ),
+                  const SizedBox(height: 10),
+                  Row(
+                    children: [
+                      IconButton(
+                        onPressed: () {
+                          setState(() {
+                            liked = !liked;
+                            likeCount += liked ? 1 : -1;
+                          });
+                        },
+                        icon: (liked
+                            ? const Icon(Icons.star)
+                            : const Icon(Icons.star_border_outlined)),
+                        color: liked ? Colors.red[500] : null,
+                      ),
+                      Text(
+                        "${likeCount} ",
+                        style:
+                            Theme.of(context).textTheme.titleMedium?.copyWith(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18,
+                                ),
+                      ),
+                    ],
+                  )
+                ],
               ),
             ),
           ],
         ),
       ),
-      /*3*/
-      Icon(
-        Icons.star,
-        color: Colors.red[500],
+    );
+  }
+}
+
+class DraggingListItem extends StatelessWidget {
+  const DraggingListItem({
+    super.key,
+    required this.dragKey,
+    required this.photoProvider,
+  });
+
+  final GlobalKey dragKey;
+  final ImageProvider photoProvider;
+
+  @override
+  Widget build(BuildContext context) {
+    return FractionalTranslation(
+      translation: const Offset(-0.5, -0.5),
+      child: ClipRRect(
+        key: dragKey,
+        borderRadius: BorderRadius.circular(12),
+        child: SizedBox(
+          height: 150,
+          width: 150,
+          child: Opacity(
+            opacity: 0.85,
+            child: Image(
+              image: photoProvider,
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
       ),
-      Icon(
-        Icons.star,
-        color: Colors.red[500],
+    );
+  }
+}
+
+class PaymentPage extends StatelessWidget {
+  const PaymentPage({
+    super.key,
+    required this.customer,
+    required this.onNextPressed,
+  });
+  final Customer customer;
+  final VoidCallback onNextPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Payment Details'),
       ),
-      Icon(
-        Icons.star,
-        color: Colors.red[500],
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Customer: ${customer.name}',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            const SizedBox(height: 8),
+            const Text('Items:'),
+            for (var item in customer.items)
+              Text('- ${item.name} : ${item.formattedTotalItemPrice}'),
+            const SizedBox(height: 8),
+            Text('Total: ${customer.formattedTotalItemPrice}'),
+            ElevatedButton(
+              onPressed: onNextPressed,
+              child: Text('Next'),
+            ),
+          ],
+        ),
       ),
-      const Text('11299'),
-    ],
-  ),
-);
+    );
+  }
+}
+
+class PaymentOptionPage extends StatelessWidget {
+  const PaymentOptionPage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Payment Options'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                textStyle: const TextStyle(fontSize: 20),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(0),
+                ),
+              ),
+              onPressed: () {
+                // Handle cash payment
+              },
+              child: const Text('Cash Payment'),
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                textStyle: const TextStyle(fontSize: 20),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(0),
+                ),
+              ),
+              onPressed: () {
+                // Handle QR code payment
+              },
+              child: const Text('QR Code Payment'),
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                textStyle: const TextStyle(fontSize: 20),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(0),
+                ),
+              ),
+              onPressed: () {
+                // Handle credit card payment
+              },
+              child: const Text('Credit Card Payment'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+@immutable
+class Item {
+  const Item({
+    required this.totalPriceCents,
+    required this.name,
+    required this.uid,
+    required this.imageProvider,
+    required this.likeCount,
+  });
+  final int totalPriceCents;
+  final String name;
+  final String uid;
+  final ImageProvider imageProvider;
+  final int likeCount;
+  String get formattedTotalItemPrice =>
+      '\$${(totalPriceCents / 100.0).toStringAsFixed(2)}';
+}
+
+class Customer {
+  Customer({
+    required this.name,
+    required this.imageProvider,
+    List<Item>? items,
+  }) : items = items ?? [];
+
+  final String name;
+  final ImageProvider imageProvider;
+  final List<Item> items;
+
+  String get formattedTotalItemPrice {
+    final totalPriceCents =
+        items.fold<int>(0, (prev, item) => prev + item.totalPriceCents);
+    return '\$${(totalPriceCents / 100.0).toStringAsFixed(2)}';
+  }
+}
