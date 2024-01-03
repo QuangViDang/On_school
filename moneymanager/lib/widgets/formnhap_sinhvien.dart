d25b75164a10539a6c6538a1c9bb42257586d8ce
Dang Quang Vi <vidangquang1203@gmail.com>
2024-01-03 07:54:37 +0700

    add student in student management

---
+++ moneymanager/lib/widgets/formnhap_sinhvien.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:date_format/date_format.dart';
+
+import '../models/sinhvien.dart';
+
+class FormNhapSinhVien extends StatefulWidget {
+  final void Function(SinhVien) onThemSinhVien;
+
+  FormNhapSinhVien({required this.onThemSinhVien});
+
+  @override
+  _FormNhapSinhVienState createState() => _FormNhapSinhVienState();
+}
+
+class _FormNhapSinhVienState extends State<FormNhapSinhVien> {
+  final maController = TextEditingController();
+  final hoVaTenController = TextEditingController();
+  late DateTime selectedDate; // Thêm biến để lưu ngày được chọn
+  final diemTotNghiepController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    selectedDate = DateTime.now();
+  }
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime picked = (await showDatePicker(
+      context: context,
+      initialDate: selectedDate,
+      firstDate: DateTime(1900),
+      lastDate: DateTime(2101),
+    ))!;
+
+    if (picked != null && picked != selectedDate) {
+      setState(() {
+        selectedDate = picked;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            TextField(
+              decoration: InputDecoration(labelText: 'Student Code'),
+              controller: maController,
+              style: GoogleFonts.notoSans(),
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Student Name'),
+              controller: hoVaTenController,
+              style: GoogleFonts.notoSans(),
+            ),
+            GestureDetector(
+              onTap: () => _selectDate(context),
+              child: AbsorbPointer(
+                child: TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Student Date of Birth',
+                  ),
+                  controller: TextEditingController(
+                    text: formatDate(
+                      selectedDate,
+                      [yyyy, '-', mm, '-', dd],
+                    ),
+                  ),
+                  style: GoogleFonts.notoSans(),
+                ),
+              ),
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Graduation'),
+              controller: diemTotNghiepController,
+              style: GoogleFonts.notoSans(),
+            ),
+            TextButton(
+              child: Text('Thêm sinh viên'),
+              onPressed: () {
+                // Tạo đối tượng SinhVien từ dữ liệu nhập
+                SinhVien sinhVien = SinhVien(
+                  ma: int.parse(maController.text),
+                  hoVaTen: hoVaTenController.text,
+                  ngaySinh: selectedDate,
+                  diemTotNghiep: double.parse(diemTotNghiepController.text),
+                  // Thêm các trường khác tùy ý
+                );
+
+                // Gọi callback để thông báo về QuanLySinhVien
+                widget.onThemSinhVien(sinhVien);
+
+                // Xóa dữ liệu nhập
+                maController.clear();
+                hoVaTenController.clear();
+                diemTotNghiepController.clear();
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ moneymanager/lib/widgets/quanly_sinhvien.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import '../models/sinhvien.dart';
+import 'danhsach_sinhvien.dart';
+import 'formnhap_sinhvien.dart';
+
+class QuanLySinhVien extends StatefulWidget {
+  @override
+  _QuanLySinhVienState createState() => _QuanLySinhVienState();
+}
+
+class _QuanLySinhVienState extends State<QuanLySinhVien> {
+  List<SinhVien> danhSachSinhVien = [
+    SinhVien(
+      ma: 12345678,
+      hoVaTen: "Nguyen Thi Huong",
+      ngaySinh: DateTime(2002, 8, 20),
+      diemTotNghiep: 8.2,
+    ),
+    SinhVien(
+      ma: 22334455,
+      hoVaTen: "Tran Van Doan",
+      ngaySinh: DateTime(1999, 12, 7),
+      diemTotNghiep: 7.9,
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        FormNhapSinhVien(onThemSinhVien: _themSinhVien),
+        DanhSachSinhVien(danhSachSinhVien),
+      ],
+    );
+  }
+
+  void _themSinhVien(SinhVien sinhVien) {
+    setState(() {
+      danhSachSinhVien.add(sinhVien);
+    });
+  }
+}
